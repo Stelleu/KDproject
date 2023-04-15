ba00717eb2d0031f3169d3d38527385cdeebbd5a
Stelleu <estelle272001@gmail.com>
2023-04-15 14:23:09 +0200

    Routing

---
+++ db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `article` (
+                           `id` int NOT NULL,
+                           `Title` varchar(1000) NOT NULL,
+                           `Image` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+                           `HeaderImage` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+                           `Introduction` mediumtext NOT NULL,
+                           `Description` text NOT NULL,
+                           `LastMod` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                           `Language` varchar(2) NOT NULL,
+                           `KeyWords` varchar(1000) NOT NULL,
+                           `State` int NOT NULL,
+                           `NumVisit` int NOT NULL,
+                           `IdTheme` int NOT NULL,
+                           `IdUser` int NOT NULL,
+                           `IdHost` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- index.php
+++ index.php
@@ -1,1 +1,38 @@
 <?php
+namespace App;
+
+function myAutoloader($class){
+    $class = str_ireplace("App\\", "", $class);
+    $class = str_ireplace("\\", "/", $class);
+    if(file_exists($class.".class.php")){
+        include $class.".class.php";
+    }
+}
+spl_autoload_register("App\myAutoloader");
+
+
+$uri = $_SERVER["REQUEST_URI"];
+if (!file_exists("route.json")){
+    die("le fichier n'existe pas");
+}
+$json = file_get_contents("route.json");
+$route = json_decode($json,true);
+if (!$route[$uri] || !$route[$uri]["controller"] || !$route[$uri]["action"] ){
+    die("URL invalide");
+}
+$controller = ucfirst(strtolower($route[$uri]["controller"]));
+$action = strtolower($route[$uri]["action"]);
+$controllerFile = "Controller/".$controller."class.php";
+if (!file_exists($controllerFile)){
+    die("Le fichier n'existe pas");
+}
+include $controllerFile;
+$controller = "App\\Controller\\".$controller;
+if (!class_exists($controller)){
+    die("La classe".$controller. " n'existe pas");
+}
+$objectController = new $controller();
+if (!method_exists($objectController,$action)){
+    die("la methode".$action."n'existe pas");
+}
+$objectController->$action();
